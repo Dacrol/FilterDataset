e40b25b061de948c6cd820f8df006c591dd10492
Dacrol <jonas.ahrens@gmail.com>
2018-03-01 22:52:56 +0100

    Add get_batch

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ class Filterer:
         self.attributes = attributes
         self.filenames = np.array()
         self.attributes_file = attributes_file
+        self.matches = 0
 
     def get_filenames(self):
         filenames = np.loadtxt(self.attributes_file,
                                dtype=str, skiprows=1, usecols=self.attributes)
         return filenames
+
+    def get_batch(self, no=64):
+        return 0
