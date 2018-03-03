288f334b9fa109e35c49c05932ba01f5888bd601
Dacrol <jonas.ahrens@gmail.com>
2018-03-03 23:03:06 +0100

    Change default attributes to empty tuple

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import numpy as np
 
 
 class Filterer:
-    def __init__(self, attributes=(0), attributes_file=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'attributes.txt'))):
+    def __init__(self, attributes=(), attributes_file=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'attributes.txt'))):
         self.attributes = attributes
         self.filenames = np.array([], dtype='str')
         self.attributes_file = attributes_file
         self.matches = 0
 
-    def get_filenames(self, attributes=(0)):
+    def get_filenames(self, attributes=()):
         with open(self.attributes_file, mode='rt') as file:
             next(file)
             possible_attributes = file.readline().strip()
         possible_attributes = possible_attributes.split(' ')
         # print(str(possible_attributes))
 
-        if (attributes == (0) and self.attributes != (0)):
+        if (attributes == () and self.attributes != ()):
             attributes = self.attributes
         filenames = np.loadtxt(self.attributes_file,
                                dtype=str, skiprows=1, usecols=attributes)
