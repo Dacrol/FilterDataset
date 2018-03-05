b419b0d15a5fe9d5c56f657378027fcdca9232e8
Dacrol <jonas.ahrens@gmail.com>
2018-03-05 02:52:42 +0100

    Calculate result from both attributes and negated attributes

--- main.py
+++ main.py
@@ -20,19 +20,34 @@ class Filterer:
 
         if (len(attributes) == 0 and len(self.attributes) != 0):
             attributes = self.attributes
-        attributes = tuple(map(lambda attribute: possible_attributes.index(
-            attribute) if type(attribute) is str else attribute, attributes))
-        attributes = (0,) + tuple(sorted([(attribute + 1)
-                                          for attribute in attributes]))
-        print(attributes)
         if (len(notattributes) == 0 and len(self.notattributes) != 0):
             notattributes = self.notattributes
         attributes, notattributes = tuple([tuple(attributeslist) if type(attributeslist) is not str else (
             attributeslist,) for attributeslist in (attributes, notattributes)])
 
+        attributes, notattributes = (tuple(map(lambda attribute: possible_attributes.index(attribute) if type(
+            attribute) is str else attribute, attributeslist)) for attributeslist in (attributes, notattributes))
+        attributes, notattributes = tuple(tuple(sorted(tuple(
+            (attribute + 1) for attribute in attributeslist))) for attributeslist in (attributes, notattributes))
+        attributes = (0, *attributes)
+        # print(attributes, notattributes)
+
         filenames = np.genfromtxt(self.attributes_file,
-                                  dtype=str, skip_header=2, usecols=attributes)
-        return np.array(list(map(lambda row: (row[0], (-1 if '-1' in row[1:] else 1)), filenames)), dtype=[('filename', 'U' + str(len(max(filenames[:, 0], key=len)))), ('label', '<i1')])
+                                  dtype=str, skip_header=2, usecols=(attributes + notattributes))
+
+        match = ['1' if attribute not in notattributes else '-1' for attribute in (
+            attributes[1:] + notattributes)]
+
+        # # Bonus generator vs list comprehension performance measurement:
+        # from timeit import default_timer as timer
+        # start = timer()
+        # list(((row[0], 1 if np.array_equal(row[1:], match) else -1) for row in filenames))
+        # t1 = timer() - start
+        # [(row[0], 1 if np.array_equal(row[1:], match) else -1) for row in filenames] # Faster
+        # t2 = timer() - t2
+        # print(t1, t2)
+
+        return np.array([(row[0], 1 if np.array_equal(row[1:], match) else -1) for row in filenames], dtype=[('filename', 'U' + str(len(max(filenames[:, 0], key=len)))), ('label', '<i1')])
 
     def get_batch(self, no=64):
         return 0
